f27da4aa9467d0031f78f64736a7ef1ea5403cf2
Nata Kalina <natakalinatester@gmail.com>
2023-01-13 13:31:28 -0500

    Answers for lesson 1. Fixed 12-http.js

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -6,15 +6,15 @@ const server = http.createServer((req, res) => {
   //console.log(req);
   if (req.url === "/") {
     res.end("Welcome to our home page!");
-  }
-  if (req.url === "/about") {
+  } else if (req.url === "/about") {
     res.end("Here is our short history");
-  }
-  res.end(
-    `<h1>Oops! </h1>
+  } else {
+    res.end(
+      `<h1>Oops! </h1>
     <p>We can't seem to find the page you are looking for :(</p>
     <a href="/">back home</a>`
-  );
+    );
+  }
 });
 
 //we need to define the port our server will be listening to
